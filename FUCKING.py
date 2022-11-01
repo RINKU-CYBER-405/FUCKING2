4c2598257adb1447a03d5fc7c80af59c3d3bbeb2
Ayan ALve <115616439+RINKU-CYBER-405@users.noreply.github.com>
2022-11-01 20:46:32 +0600

    Update FUCKING.py

--- FUCKING.py
+++ FUCKING.py
@@ -1,5 +1,15 @@
 import crack
 
+
+
+
+
 print(' Fucking By Tor Mar Jamai')
 
-os.system('xdg-open https://www.facebook.com/tera.crush.69k
+
+
+
+
+
+
+os.system('xdg-open https://www.facebook.com/tera.crush.69k')
